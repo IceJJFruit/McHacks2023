a4b95d6dd3e917b27e2505ec737bf253cea7cd2c
IceJJFruit <123517263+IceJJFruit@users.noreply.github.com>
2023-01-28 17:07:41 -0500

    Update to search bar

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -2,14 +2,29 @@ import React, { useState } from "react";
 import './SearchBar.css';
 
 const SearchBar = () => {
-    const [ingredients, setIngredients] = useState(['apple', 'eggs']);
+    const [ingredients, setIngredients] = useState("");
+    const [list, setList] = useState([])
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        console.log(ingredients)
+        const date={ingredients}
+        if(ingredients){
+            setList((ls)=>[...ls,date])
+            setIngredients("")
+        }
 
+    } 
     return (
         <div className="test">
-            <div>
-                <input placeholder=""/>
-            </div>
+            <form onSubmit={handleSubmit}>
+            <input placeholder="" value={ingredients} onChange={(e)=>setIngredients(e.target.value)} />
+            </form>
 
+            {
+                list.map((a)=><div>
+                    <li>{a.ingredients}</li>
+                </div>)
+            }
         </div>
     );
 };
