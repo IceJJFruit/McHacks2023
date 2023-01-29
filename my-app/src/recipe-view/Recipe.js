a40d5223f24f7ee099b2a5a72901127e4c383806
Rei Kong <reiikongs@gmail.com>
2023-01-29 00:58:02 -0500

    recipe add

---
+++ my-app/src/recipe-view/Recipe.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+//import ./Recipe.css
+
+function getRecipe() {
+    var recipeString = ""
+    recipeString.split(" ");
+
+    for (var i=0; i < recipeString.length; i++) {
+        // console.log(ingredients[i])
+        
+        
+    }
+}
+
+function displayRecipe() {
+    
+
+    render(); {
+        return (
+            <div>
+                <h1>Replace with Recipe name variable</h1>
+                <h2>Ingredients</h2>
+                <ul>
+                    <li>a</li>
+                    <li>b</li>
+                </ul>
+                <h2>Instructions</h2>
+                <ol>
+                    <li>e</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+export default Recipe
---
+++ my-app/src/recipe-view/RecipeList.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+class RecipeList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Replace with Recipe name variable</h1>
+                <h2>Ingredients</h2>
+                <ul>
+                    <li>a</li>
+                    <li>b</li>
+                </ul>
+                <h2>Instructions</h2>
+                <ol>
+                    <li>e</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+export default RecipeList
