827532665fc90c81b774c45b5f4404de470fbf67
IceJJFruit <123517263+IceJJFruit@users.noreply.github.com>
2023-01-28 19:33:20 -0500

    implemented tags

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import MakeURL from './components/MakeURL';
 import './App.css';
-import SearchBar from "./components/SearchBar"
+import TagsInputs from "./components/TagsInputs"
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ingredients: <SearchBar/>
+          Ingredients: <TagsInputs/>
           <MakeURL/>
         </p>
         
---
+++ my-app/src/components/TagsInputs.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import './TagsInputs.css';
+
+function TagsInput() {
+    const [tags, setTags] = useState([])
+
+    function handleKeyDown(e){
+        if(e.key !== 'Enter') return
+        const value = e.target.value
+        if(!value.trim()) return
+        setTags([...tags, value])
+        e.target.value = ''
+    }
+
+    function removeTag(index){
+        setTags(tags.filter((el, i) => i !== index))
+    }
+    return (
+        <div className="tags-input-container">
+            {/*<div className="tag-item">
+                <span className="text">hello</span>
+                <span className="text">&times;</span>
+            </div> */}
+            { tags.map((tag,index) => (
+                <div className="tag-item" key={index}>
+                    <span className="text">{tag}</span>
+                    <span className="close" onClick={() => removeTag(index)}>&times;</span>
+            </div>
+            ))}
+            <input onKeyDown={handleKeyDown} type="text" className="tags-input" placeholder="Type something"/>
+        </div>
+    )
+}
+
+export default TagsInput
